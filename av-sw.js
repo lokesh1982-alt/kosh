ed3f94379480852b93d41e545f69222ffd57b0d6
lokesh1982-alt <goset.lokesh@gmail.com>
2026-07-26 10:47:13 +0100

    Add files via upload

---
+++ av-sw.js
@@ -0,0 +1,16 @@
+// Service Worker - Cache disabled for development
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // No caching - always fetch from network
+  e.respondWith(fetch(e.request));
+});
