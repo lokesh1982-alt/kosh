7126a802781cde8446ee281b8e44e4af933f6dcd
lokesh1982-alt <goset.lokesh@gmail.com>
2026-09-21 22:00:18 +0100

    Add files via upload

---
+++ kosh-sw.js
@@ -0,0 +1,15 @@
+// Service Worker - Cache disabled for development
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
